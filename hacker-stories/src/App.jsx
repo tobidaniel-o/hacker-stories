dbc833fc2e41940e885106f10a471bb237641dab
Tobi Okunuga <tobi.daniel2018@gmail.com>
2023-07-23 12:13:35 -0400

    React JSX

--- hacker-stories/src/App.jsx
+++ hacker-stories/src/App.jsx
@@ -1,8 +1,15 @@
 function App() {
-	const title = "React";
+	const welcome = {
+		greeting: "Hey",
+		title: "React",
+	};
 	return (
 		<div>
-			<h1>Hello React</h1>
+			<h1>
+				{welcome.greeting} {welcome.title}
+			</h1>
+			<label htmlFor="search">Search: </label>
+			<input id="search" type="text" />
 		</div>
 	);
 }
